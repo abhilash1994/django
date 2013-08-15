55fa8b39dbf4e637fe0fe51f1876af2fa9d5a1f3
betaglide <anshul@ubuntu.(none)>
2013-08-15 13:31:29 -0700

    Initial commit done

---
+++ django-bolla/django_bookmarks/django_bookmarks/urls.py
@@ -0,0 +1,11 @@
+from django.conf.urls.defaults import *
+from bookmarks.views import *
+from django.contrib import admin
+
+admin.autodiscover()
+
+urlpatterns = patterns('',
+	(r'^$',main_page),
+	(r'^user/(\w+)/$',user_page),
+	(r'^admin/', include(admin.site.urls)),
+)
